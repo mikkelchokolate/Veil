16c1053c2f8223cb24589f639529252b400df318
mikkelchokolate <mikkelchokolate@users.noreply.github.com>
2026-05-07 09:24:20 +0000

    refactor: extract Management routes module

--- internal/api/management.go
+++ internal/api/management.go
@@ -261,20 +261,7 @@ func newManagementState(info ServerInfo) *managementState {
 }
 
 func (s *managementState) register(mux *http.ServeMux) {
-	mux.HandleFunc("/api/settings", s.handleSettings)
-	mux.HandleFunc("/api/inbounds", s.handleInbounds)
-	mux.HandleFunc("/api/inbounds/", s.handleInboundByName)
-	mux.HandleFunc("/api/routing/rules", s.handleRoutingRules)
-	mux.HandleFunc("/api/routing/rules/", s.handleRoutingRuleByName)
-	mux.HandleFunc("/api/routing/presets", s.handleRoutingPresets)
-	mux.HandleFunc("/api/routing/presets/", s.handleRoutingPresetByName)
-	mux.HandleFunc("/api/warp", s.handleWarp)
-	mux.HandleFunc("/api/client-links/subscription", s.handleClientLinksSubscription)
-	mux.HandleFunc("/api/client-links", s.handleClientLinks)
-	mux.HandleFunc("/api/firewall", s.handleFirewall)
-	mux.HandleFunc("/api/apply/plan", s.handleApplyPlan)
-	mux.HandleFunc("/api/apply/history", s.handleApplyHistory)
-	mux.HandleFunc("/api/apply", s.handleApply)
+	ManagementRoutes{}.Register(mux, s)
 }
 
 func (s *managementState) handleSettings(w http.ResponseWriter, r *http.Request) {
