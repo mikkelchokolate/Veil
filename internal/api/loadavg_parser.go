144177d9583e0b074d1df70be5800012b778854a
mikkelchokolate <mikkelchokolate@users.noreply.github.com>
2026-05-07 19:19:09 +0000

    refactor: extract Load average parser module

---
+++ internal/api/loadavg_parser.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"strconv"
+	"strings"
+)
+
+type LoadAvgParser struct{}
+
+func NewLoadAvgParser() LoadAvgParser { return LoadAvgParser{} }
+
+func (LoadAvgParser) Parse(body string) loadAvg {
+	fields := strings.Fields(body)
+	if len(fields) < 3 {
+		return loadAvg{}
+	}
+	avg1, _ := strconv.ParseFloat(fields[0], 64)
+	avg5, _ := strconv.ParseFloat(fields[1], 64)
+	avg15, _ := strconv.ParseFloat(fields[2], 64)
+	return loadAvg{avg1: avg1, avg5: avg5, avg15: avg15}
+}
--- internal/api/system.go
+++ internal/api/system.go
@@ -82,14 +82,7 @@ func readLoadAvg() (loadAvg, error) {
 	if err != nil {
 		return loadAvg{}, err
 	}
-	fields := strings.Fields(string(data))
-	if len(fields) < 3 {
-		return loadAvg{}, nil
-	}
-	avg1, _ := strconv.ParseFloat(fields[0], 64)
-	avg5, _ := strconv.ParseFloat(fields[1], 64)
-	avg15, _ := strconv.ParseFloat(fields[2], 64)
-	return loadAvg{avg1: avg1, avg5: avg5, avg15: avg15}, nil
+	return NewLoadAvgParser().Parse(string(data)), nil
 }
 
 func parseKB(line string) int64 {
