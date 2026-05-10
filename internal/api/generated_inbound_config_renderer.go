152da40c9cb0ae54472dcc0973b3abb020845157
mikkelchokolate <mikkelchokolate@users.noreply.github.com>
2026-05-10 00:19:33 +0000

    refactor: remove inbound renderer wrapper

--- internal/api/generated_inbound_config_renderer.go
+++ internal/api/generated_inbound_config_renderer.go
@@ -4,30 +4,6 @@ import "github.com/veil-panel/veil/internal/generatedconfig"
 
 type GeneratedConfigArtifact = generatedconfig.GeneratedConfigArtifact
 
-type GeneratedInboundConfigRenderer struct {
-	settings Settings
-	paths    GeneratedConfigPaths
-}
-
-func NewGeneratedInboundConfigRenderer(settings Settings, paths GeneratedConfigPaths) GeneratedInboundConfigRenderer {
-	return GeneratedInboundConfigRenderer{settings: settings, paths: paths}
-}
-
-func (r GeneratedInboundConfigRenderer) Render(inbound Inbound) (GeneratedConfigArtifact, bool, error) {
-	if !inbound.Enabled {
-		return GeneratedConfigArtifact{}, false, nil
-	}
-	return NewGeneratedConfigProtocolRegistry().RenderInbound(r.settings, r.paths, inbound)
-}
-
-func (r GeneratedInboundConfigRenderer) renderNaive(inbound Inbound) (string, error) {
-	return generatedconfig.NewInboundRenderer(r.settings, r.paths).RenderNaive(inbound)
-}
-
-func (r GeneratedInboundConfigRenderer) renderHysteria2(inbound Inbound) (string, error) {
-	return generatedconfig.NewInboundRenderer(r.settings, r.paths).RenderHysteria2(inbound)
-}
-
 func renderNaiveGeneratedConfig(settings Settings, inbound Inbound) (string, error) {
 	return generatedconfig.RenderNaiveInbound(settings, inbound)
 }
