92c94704b2d1f948aa6b4a2b1e62bad5d6a7352f
mikkelchokolate <mikkelchokolate@users.noreply.github.com>
2026-05-10 00:43:31 +0000

    refactor: trim config cardinality adapter

--- internal/api/generated_config_cardinality.go
+++ internal/api/generated_config_cardinality.go
@@ -2,18 +2,6 @@ package api
 
 import "github.com/veil-panel/veil/internal/generatedconfig"
 
-type GeneratedConfigCardinality struct {
-	inner generatedconfig.GeneratedConfigCardinality
-}
-
-func NewGeneratedConfigCardinality(settings Settings) GeneratedConfigCardinality {
-	return GeneratedConfigCardinality{inner: generatedconfig.NewGeneratedConfigCardinality(settings, NewGeneratedConfigProtocolRegistry().inner)}
-}
-
-func (c GeneratedConfigCardinality) Validate(inbounds []Inbound) error {
-	return c.inner.Validate(inbounds)
-}
-
 func validateGeneratedConfigInboundCardinality(settings Settings, inbounds []Inbound) error {
-	return NewGeneratedConfigCardinality(settings).Validate(inbounds)
+	return generatedconfig.NewGeneratedConfigCardinality(settings, NewGeneratedConfigProtocolRegistry().inner).Validate(inbounds)
 }
