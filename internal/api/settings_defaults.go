b65904ca712cf6c8e09f0cbfaf335cec56210c91
mikkelchokolate <mikkelchokolate@users.noreply.github.com>
2026-05-10 00:24:04 +0000

    refactor: remove unused settings defaults wrapper

--- internal/api/settings_defaults.go
+++
@@ -1,7 +0,0 @@
-package api
-
-import "github.com/veil-panel/veil/internal/managementstate"
-
-func mergeSettingsDefaults(settings Settings, defaults Settings) Settings {
-	return managementstate.MergeSettingsDefaults(settings, defaults)
-}
