51307ea8f8fdfcf7ffa137f29c6bd497886f9df6
mikkelchokolate <mikkelchokolate@users.noreply.github.com>
2026-05-10 00:41:43 +0000

    refactor: trim service policy adapters

--- internal/api/service_health_collection.go
+++ internal/api/service_health_collection.go
@@ -2,23 +2,8 @@ package api
 
 import "github.com/veil-panel/veil/internal/service"
 
-type ServiceHealthCollection struct {
-	inner service.ServiceHealthCollection
-}
-
-func NewServiceHealthCollection(check func(name string) ServiceHealthResult) ServiceHealthCollection {
-	if check == nil {
-		check = serviceHealthChecker
-	}
-	return ServiceHealthCollection{inner: service.NewServiceHealthCollection(func(name string) ServiceHealthResult {
-		return check(name)
-	})}
-}
-
-func (c ServiceHealthCollection) Check(actions []ServiceActionResult) []ServiceHealthResult {
-	return c.inner.Check(actions)
-}
-
 func checkServiceHealth(actions []ServiceActionResult) []ServiceHealthResult {
-	return NewServiceHealthCollection(serviceHealthChecker).Check(actions)
+	return service.NewServiceHealthCollection(func(name string) ServiceHealthResult {
+		return serviceHealthChecker(name)
+	}).Check(actions)
 }
