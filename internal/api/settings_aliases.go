a7cb28af7c83d357bfe5b9aae044147f13a09401
mikkelchokolate <mikkelchokolate@users.noreply.github.com>
2026-05-10 00:25:56 +0000

    refactor: trim settings aliases

--- internal/api/settings_aliases.go
+++ internal/api/settings_aliases.go
@@ -2,24 +2,6 @@ package api
 
 import veilsettings "github.com/veil-panel/veil/internal/settings"
 
-const RedactedSecret = veilsettings.RedactedSecret
-
 type CredentialDisclosure = veilsettings.CredentialDisclosure
-type SettingsValidation = veilsettings.SettingsValidation
-type SettingsRedaction = veilsettings.SettingsRedaction
 
 func NewCredentialDisclosure() CredentialDisclosure { return veilsettings.NewCredentialDisclosure() }
-func NewSettingsValidation() SettingsValidation     { return veilsettings.NewSettingsValidation() }
-func NewSettingsRedaction() SettingsRedaction       { return veilsettings.NewSettingsRedaction() }
-
-func normalizeAndValidateSettings(settings *Settings, current Settings) error {
-	return veilsettings.NewSettingsValidation().NormalizeAndValidate(settings, current)
-}
-
-func redactedSettings(settings Settings) Settings {
-	return veilsettings.NewSettingsRedaction().Redact(settings)
-}
-
-func normalizeSettingsWebBasePath(path string) string {
-	return veilsettings.NormalizeWebBasePath(path)
-}
