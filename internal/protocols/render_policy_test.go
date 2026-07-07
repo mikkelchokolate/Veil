ebdf843fbc102051625b52c1b82cce1f8f625697
mikkelchokolate <mikkelchokolate@gmail.com>
2026-07-07 23:32:33 +0300

    test: use existing protocol mock

--- internal/protocols/render_policy_test.go
+++ internal/protocols/render_policy_test.go
@@ -3,21 +3,21 @@ package protocols
 import "testing"
 
 type renderPolicyPlugin struct {
-	mockProtocol
+	*mockPlugin
 	requires bool
 }
 
 func (p renderPolicyPlugin) RequiresRenderSettings() bool { return p.requires }
 
 func TestRequiresRenderSettingsDefault(t *testing.T) {
-	plugin := mockProtocol{protocol: "default-render-policy", transports: []string{"tcp"}}
+	plugin := &mockPlugin{protocol: "default-render-policy", transports: []string{"tcp"}}
 	if !RequiresRenderSettings(plugin) {
 		t.Fatal("expected default true")
 	}
 }
 
 func TestRequiresRenderSettingsOverride(t *testing.T) {
-	plugin := renderPolicyPlugin{mockProtocol: mockProtocol{protocol: "local-render", transports: []string{"tcp"}}, requires: false}
+	plugin := renderPolicyPlugin{mockPlugin: &mockPlugin{protocol: "local-render", transports: []string{"tcp"}}, requires: false}
 	if RequiresRenderSettings(plugin) {
 		t.Fatal("expected override false")
 	}
