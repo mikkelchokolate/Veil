120516d38844aaee22af06899dc65749bf148f34
mikkelchokolate <mikkelchokolate@gmail.com>
2026-09-20 14:34:43 +0300

    test(browser-e2e): users page landmark is panelUsers for admins

--- test/browser/panel-nav-smoke.spec.js
+++ test/browser/panel-nav-smoke.spec.js
@@ -18,7 +18,8 @@ const NAV_SMOKE = [
   { link: /^warp$/i, url: /\/warp$/, heading: /^warp outbound$/i },
   { link: /^system$/i, url: /\/system$/, heading: /^system$/i },
   { link: /^backups$/i, url: /\/backups$/, heading: /^backups$/i },
-  { link: /^users$/i, url: /\/users$/, heading: /^users$/i },
+  // Admins get the management view (users.panelUsers); viewers get users.title.
+  { link: /^users$/i, url: /\/users$/, heading: /panel users|^users$/i },
   { link: /^settings$/i, url: /\/settings$/, heading: /^settings$/i },
   { link: /^apply$/i, url: /\/apply$/, heading: /^apply state$/i },
 ];
